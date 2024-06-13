bcda50deaa018683fb3381b087f205a0cacabead
unknown <tomfraczekdev@gmail.com>
2024-06-13 13:49:30 +0200

    feat: add MainNav component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { MobileNav } from "./MobileNav";
+import { MainNav } from "./MainNav";
 
 export const Header = () => (
   <div className="border-b-2 border-b-orange-500 py-6">
@@ -12,6 +13,9 @@ export const Header = () => (
       </Link>
       <div className="md:hidden">
         <MobileNav />
+      </div>
+      <div className="hidden md:block">
+        <MainNav />
       </div>
     </div>
   </div>
---
+++ src/components/MainNav.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./ui/button";
+
+export const MainNav = () => (
+  <Button
+    variant="ghost"
+    className="font-bold hover:text-orange-500 hover:bg-white"
+  >
+    Log In
+  </Button>
+);
