2fb06a2cf74ea1f835b1be41e18e8dcd80d56a36
unknown <tomfraczekdev@gmail.com>
2024-06-13 13:58:02 +0200

    feat: add homepage assets and hero component

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
 
 type Props = {
   children: React.ReactNode;
@@ -7,7 +8,7 @@ type Props = {
 const Layout = ({ children }: Props) => (
   <div className="flex flex-col min-h-screen">
     <Header />
-
+    <Hero />
     <div className="container mx-auto flex-1 py-10">{children}</div>
   </div>
 );
