076839575538a9638d3c45e35fc29dbfc47b6175
unknown <tomfraczekdev@gmail.com>
2024-06-13 15:41:00 +0200

    feat: add and config Auth0 authentication

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,10 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
-export const MainNav = () => (
-  <Button
-    variant="ghost"
-    className="font-bold hover:text-orange-500 hover:bg-white"
-  >
-    Log In
-  </Button>
-);
+export const MainNav = () => {
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold hover:text-orange-500 hover:bg-white"
+      onClick={async () => await loginWithRedirect()}
+    >
+      Log In
+    </Button>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouts from "./AppRoutes";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AppRouts />
+      <Auth0ProviderWithNavigate>
+        <AppRouts />
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>
 );
