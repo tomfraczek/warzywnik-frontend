20ce66f455f5b8b9082c371ef7f5fbd96326677f
unknown <tomfraczekdev@gmail.com>
2024-06-20 12:33:01 +0200

    chore: code cleanup

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import { HomePage } from "./pages/HomePage";
+import { AuthCallbackPage } from "./pages/AuthCallbackPage";
 
 const AppRouts = () => (
   <Routes>
@@ -12,6 +13,7 @@ const AppRouts = () => (
         </Layout>
       }
     />
+    <Route path="/auth-callback" element={<AuthCallbackPage />} />
     <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
     <Route path="*" element={<Navigate to="/" />} />
   </Routes>
